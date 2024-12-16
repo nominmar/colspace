3a72b4c7c120104f5312c7fe6df895ff183e2dca
Nomin <nominmar@gmail.com>
2024-12-16 16:13:58 +0000

    cosmetic stuff + moving random rgb generation to the game and restart game if game options are updated

--- app_utils.py
+++ app_utils.py
@@ -51,37 +51,54 @@ def plot_plane(fig, ax_, best_guess, last_guess):
         ax_.plot(best_guess.x, best_guess.y, 'x', markersize=8, color='red', label=f'Best Guess', mew=2, path_effects=[path_effects.withStroke(linewidth=4, foreground='black')])
     if last_guess:
         ax_.plot(last_guess.x, last_guess.y, '+', markersize=8, color='black', label=f'Latest guess', mew=2, path_effects=[path_effects.withStroke(linewidth=4, foreground='black')])
+        ax_.axhline(y=last_guess.y, color='black', linestyle='-.', linewidth=0.5)
+        ax_.axvline(x=last_guess.x, color='black', linestyle='-.', linewidth=0.5)
         ax_.legend()
     return fig
 
 def get_game_settings():
     with st.sidebar:
         st.write('### Game Settings')
-        max_tolerance = st.number_input("Max Tolerance", min_value=0.001, max_value=0.1, value=0.01)
-        max_tries = st.number_input("Max Number of Tries", min_value=1, max_value=20, value=10)
-        difficulty = st.selectbox('Select the difficulty:', ['Easy', 'Medium'], index=1)
+        max_tolerance = st.number_input("Max Error Tolerance", min_value=0.01, max_value=0.1, value=0.01)
+        st.markdown('<small>You win if Euclidian distance to target is less than this</small>', unsafe_allow_html=True)
+        max_tries = st.number_input("Max Number of Tries", min_value=1, max_value=100, value=20)
+        difficulty = st.selectbox('Select the difficulty:', ['Easy', 'Medium', 'Hard'], index=1)
+        st.markdown('<small>Medium and hard will ensure target color is not at the edges of sRGB space </small>', unsafe_allow_html=True)
     return max_tolerance, max_tries, difficulty
 
 def set_session_state(max_tries=10, max_tolerance=0.01, level='Medium'):
     if 'rgb' not in st.session_state:
         if level == 'Medium':
-            st.session_state.rgb = generate_random_rgb(max_value=180)
+            st.session_state.rgb = generate_random_rgb(max_value=170)
+        elif level == 'Hard':
+            st.session_state.rgb = generate_random_rgb(max_value=100)
         else:
             st.session_state.rgb = generate_random_rgb()
-    if 'game_' not in st.session_state:
-        st.session_state.game_ = ColorSpaces(target_color=st.session_state.rgb,
+    if ('game_' not in st.session_state): 
+        st.session_state.game_ = ColorSpaces(
                                          max_guesses=max_tries,
-                                         max_tolerance=max_tolerance)
+                                         max_tolerance=max_tolerance, 
+                                         level=level)
+    elif  (max_tolerance != st.session_state.game_.max_tolerance) | (max_tries != st.session_state.game_.max_guesses) | (st.session_state.game_.level != level):
+        st.session_state.game_ = ColorSpaces(
+                                         max_guesses=max_tries,
+                                         max_tolerance=max_tolerance,
+                                         level=level)
     if '_plane' not in st.session_state:
         st.session_state.plane, st.session_state.ax = plot_cie1931((st.session_state.game_.target_x, st.session_state.game_.target_y)) 
+
+def game_faq():
+    with st.expander('Game movement FAQ', expanded=False):
+        with open("./data/faq.html", "r") as file:
+            faq_html = file.read()
+            st.components.v1.html(faq_html, height=500)
 
 def game_instructions():
     with st.container():
         st.markdown('---')
-        st.markdown('### Color Space Game - Instructions')
-        st.markdown('''
-            Random color is generated and placed on CIE 1931 chromaticity diagram. (White X is the target color) \n
-            You can guess the color in either RGB or CMYK color spaces. (Your guess will be displayed on the chromaticity diagram) \n
-            Update the tolerance on sidebar, once you are close enough to the target color, it will stop. \n
-        ''')
+        st.markdown('<h3>Color Space Game - Instructions 🎮🎨</h3>', unsafe_allow_html=True)
+        with open("./data/intro.html", "r") as file:
+            instructions = file.read()
+            st.components.v1.html(instructions, height=100)
+        game_faq()
         st.markdown('---')
